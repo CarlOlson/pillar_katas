c31c816e0ee642dfd1b57dca0eba8922934c2d8d
Carl Olson <carl.anders.olson@gmail.com>
2016-11-08 13:43:41 -0600

    describe red pencil acceptance tests

---
+++ red_pencil/spec/product_spec.rb
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+describe Product do
+  describe "red pencil promotion" do
+    # A red pencil promotion lasts 30 days as the maximum length.
+    it "should last 30 days at most"
+    it "should last 30 days by default"
+
+    # A red pencil promotion starts due to a price reduction. The price
+    # has to be reduced by at least 5% but at most bei 30% and the
+    # previous price had to be stable for at least 30 days.
+    it "should start due to a price reduction"
+    it "should start after a >=5% reduction"
+    it "should not start after a >30% reduction"
+    it "should only start after the previous price was stable for >=30 days"
+
+    # If the price is further reduced during the red pencil promotion the
+    # promotion will not be prolonged by that reduction.
+    it "should not be extended upon additional reductions"
+
+    # If the price is increased during the red pencil promotion the
+    # promotion will be ended immediately.
+    it "should end if the price is increased"
+
+    # If the price if reduced during the red pencil promotion so that the
+    # overall reduction is more than 30% with regard to the original
+    # price, the promotion is ended immediately.
+    it "should end after additional reductions equal >30% of the original price"
+
+    # After a red pencil promotion is ended additional red pencil
+    # promotions may follow - as long as the start condition is valid: the
+    # price was stable for 30 days and these 30 days don’t intersect with
+    # a previous red pencil promotion.
+    it "should not happen again until 30 days after the last promotion"
+  end
+end

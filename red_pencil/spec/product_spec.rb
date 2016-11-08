915e085f232fd71c5148742acc091b9ed3e270e0
Carl Olson <carl.anders.olson@gmail.com>
2016-11-08 14:18:44 -0600

    implement acceptance tests

--- red_pencil/spec/product_spec.rb
+++ red_pencil/spec/product_spec.rb
@@ -5,41 +5,9 @@ require "product"
 describe Product do
   before do
     @product = Product.new 100
+    @product.day = 30
   end
-  
-  describe "red pencil promotion" do
-    # A red pencil promotion lasts 30 days as the maximum length.
-    it "should last 30 days at most"
-    it "should last 30 days by default"
 
-    # A red pencil promotion starts due to a price reduction. The price
-    # has to be reduced by at least 5% but at most bei 30% and the
-    # previous price had to be stable for at least 30 days.
-    it "should start due to a price reduction"
-    it "should start after a >=5% reduction"
-    it "should not start after a >30% reduction"
-    it "should only start after the previous price was stable for >=30 days"
-
-    # If the price is further reduced during the red pencil promotion the
-    # promotion will not be prolonged by that reduction.
-    it "should not be extended upon additional reductions"
-
-    # If the price is increased during the red pencil promotion the
-    # promotion will be ended immediately.
-    it "should end if the price is increased"
-
-    # If the price if reduced during the red pencil promotion so that the
-    # overall reduction is more than 30% with regard to the original
-    # price, the promotion is ended immediately.
-    it "should end after additional reductions equal >30% of the original price"
-
-    # After a red pencil promotion is ended additional red pencil
-    # promotions may follow - as long as the start condition is valid: the
-    # price was stable for 30 days and these 30 days don’t intersect with
-    # a previous red pencil promotion.
-    it "should not happen again until 30 days after the last promotion"
-  end
-  
   describe "#price=" do
     it "should update the current price" do
       @product.price = 110
@@ -47,6 +15,101 @@ describe Product do
     end
 
     it "should trigger a red pencil promotion" do
+      @product.price = 90
+      @product.red_pencil?.should be_true
+    end
+  end
+  
+  describe "red pencil promotion" do
+    # A red pencil promotion lasts 30 days as the maximum length.
+    it "should last 30 days at most" do
+      @product.price = 90
+      @product.red_pencil?.should be_true
+
+      @product.day += 30
+      @product.red_pencil?.should be_false
+    end
+
+    # A red pencil promotion starts due to a price reduction. The price
+    # has to be reduced by at least 5% but at most bei 30% and the
+    # previous price had to be stable for at least 30 days.
+    it "should start due to a price reduction" do
+      @product.price = 90
+      @product.red_pencil?.should be_true
+    end
+    it "should start after a >=5% reduction" do
+      @product.price = 96
+      @product.red_pencil?.should be_false
+
+      @product.price = 95
+      @product.red_pencil?.should be_true
+    end
+    it "should not start after a >30% reduction" do
+      @product.price = 69
+      @product.red_pencil?.should be_false
+    end
+    it "should only start after the previous price was stable for >=30 days" do
+      @product.price = 110
+      @product.day += 29
+      @product.price = 100
+      @product.red_pencil?.should be_false
+
+      @product.day += 30
+      @product.price = 90
+      @product.red_pencil?.should be_true
+    end
+
+    # If the price is further reduced during the red pencil promotion the
+    # promotion will not be prolonged by that reduction.
+    it "should not be extended upon additional reductions" do
+      @product.price = 90
+      @product.day += 15
+      @product.red_pencil?.should be_true
+      
+      @product.price = 80
+      @product.day += 15
+      @product.red_pencil?.should be_false
+    end
+
+    # If the price is increased during the red pencil promotion the
+    # promotion will be ended immediately.
+    it "should end if the price is increased" do
+      @product.price = 90
+      @product.red_pencil?.should be_true
+      
+      @product.price += 10
+      @product.red_pencil?.should be_false
+    end
+
+    # If the price if reduced during the red pencil promotion so that the
+    # overall reduction is more than 30% with regard to the original
+    # price, the promotion is ended immediately.
+    it "should end after additional reductions equal >30% of the original price" do
+      @product.price = 90
+      @product.red_pencil?.should be_true
+
+      @product.day += 1
+      @product.price = 69
+      @product.red_pencil?.should be_false
+    end
+
+    # After a red pencil promotion is ended additional red pencil
+    # promotions may follow - as long as the start condition is valid: the
+    # price was stable for 30 days and these 30 days don’t intersect with
+    # a previous red pencil promotion.
+    it "should not happen again until 30 days after the last promotion" do
+      @product.price = 90
+      @product.red_pencil?.should be_true
+
+      @product.day += 1
+      @product.price += 10
+      @product.red_pencil?.should be_false
+
+      @product.day += 29
+      @product.price = 90
+      @product.red_pencil?.should be_false
+
+      @product.day += 30
       @product.price = 90
       @product.red_pencil?.should be_true
     end

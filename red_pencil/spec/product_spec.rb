d66a4d86922b686aff2b8696e895c02e4032d412
Carl Olson <carl.anders.olson@gmail.com>
2016-11-08 14:33:48 -0600

    fix expect syntax with booleans

--- red_pencil/spec/product_spec.rb
+++ red_pencil/spec/product_spec.rb
@@ -16,7 +16,7 @@ describe Product do
 
     it "should trigger a red pencil promotion" do
       @product.price = 90
-      expect(@product.red_pencil?).to eq be_true
+      expect(@product).to be_red_pencil
     end
   end
   
@@ -24,10 +24,10 @@ describe Product do
     # A red pencil promotion lasts 30 days as the maximum length.
     it "should last 30 days at most" do
       @product.price = 90
-      expect(@product.red_pencil?).to eq be_true
+      expect(@product).to be_red_pencil
 
       @product.day += 30
-      expect(@product.red_pencil?).to eq be_false
+      expect(@product).to_not be_red_pencil
     end
 
     # A red pencil promotion starts due to a price reduction. The price
@@ -35,28 +35,28 @@ describe Product do
     # previous price had to be stable for at least 30 days.
     it "should start due to a price reduction" do
       @product.price = 90
-      expect(@product.red_pencil?).to eq be_true
+      expect(@product).to be_red_pencil
     end
     it "should start after a >=5% reduction" do
       @product.price = 96
-      expect(@product.red_pencil?).to eq be_false
+      expect(@product).to_not be_red_pencil
 
       @product.price = 95
-      expect(@product.red_pencil?).to eq be_true
+      expect(@product).to be_red_pencil
     end
     it "should not start after a >30% reduction" do
       @product.price = 69
-      expect(@product.red_pencil?).to eq be_false
+      expect(@product).to_not be_red_pencil
     end
     it "should only start after the previous price was stable for >=30 days" do
       @product.price = 110
       @product.day += 29
       @product.price = 100
-      expect(@product.red_pencil?).to eq be_false
+      expect(@product).to_not be_red_pencil
 
       @product.day += 30
       @product.price = 90
-      expect(@product.red_pencil?).to be_true
+      expect(@product).to be_red_pencil
     end
 
     # If the price is further reduced during the red pencil promotion the
@@ -64,21 +64,21 @@ describe Product do
     it "should not be extended upon additional reductions" do
       @product.price = 90
       @product.day += 15
-      expect(@product.red_pencil?).to be_true
+      expect(@product).to be_red_pencil
       
       @product.price = 80
       @product.day += 15
-      expect(@product.red_pencil?).to be_false
+      expect(@product).to_not be_red_pencil
     end
 
     # If the price is increased during the red pencil promotion the
     # promotion will be ended immediately.
     it "should end if the price is increased" do
       @product.price = 90
-      expect(@product.red_pencil?).to be_true
+      expect(@product).to be_red_pencil
       
       @product.price += 10
-      expect(@product.red_pencil?).to be_false
+      expect(@product).to_not be_red_pencil
     end
 
     # If the price if reduced during the red pencil promotion so that the
@@ -86,11 +86,11 @@ describe Product do
     # price, the promotion is ended immediately.
     it "should end after additional reductions equal >30% of the original price" do
       @product.price = 90
-      expect(@product.red_pencil?).to be_true
+      expect(@product).to be_red_pencil
 
       @product.day += 1
       @product.price = 69
-      expect(@product.red_pencil?).to be_false
+      expect(@product).to_not be_red_pencil
     end
 
     # After a red pencil promotion is ended additional red pencil
@@ -99,19 +99,19 @@ describe Product do
     # a previous red pencil promotion.
     it "should not happen again until 30 days after the last promotion" do
       @product.price = 90
-      expect(@product.red_pencil?).to be_true
+      expect(@product).to be_red_pencil
 
       @product.day += 1
       @product.price += 10
-      expect(@product.red_pencil?).to be_false
+      expect(@product).to_not be_red_pencil
 
       @product.day += 29
       @product.price = 90
-      expect(@product.red_pencil?).to be_false
+      expect(@product).to_not be_red_pencil
 
       @product.day += 30
       @product.price = 90
-      expect(@product.red_pencil?).to be_true
+      expect(@product).to be_red_pencil
     end
   end
 end

02c2fe246645ccdaedfbb237f2fe75438b72ce8e
Carl Olson <carl.anders.olson@gmail.com>
2016-11-21 00:46:14 -0600

    change rule_map to have symbols instead of procs

--- yahtzee/lib/main.rb
+++ yahtzee/lib/main.rb
@@ -2,31 +2,29 @@ module Yahtzee
   module_function
 
   SINGLES_MAP = {
-    ones: 1,
-    twos: 2,
+    ones:   1,
+    twos:   2,
     threes: 3,
-    fours: 4,
-    fives: 5,
-    sixes: 6
+    fours:  4,
+    fives:  5,
+    sixes:  6
   }.freeze
 
-  SINGLES_PROC =
-    proc { |*args| rule_singles(*args) }
-
   RULE_MAP = {
-    ones:   SINGLES_PROC,
-    twos:   SINGLES_PROC,
-    threes: SINGLES_PROC,
-    fours:  SINGLES_PROC,
-    fives:  SINGLES_PROC,
-    sixes:  SINGLES_PROC,
-    pair:   proc { |*args| rule_pair(*args) },
-    two_pair: proc { |*args| rule_two_pair(*args) },
-    fullhouse: proc { |*args| rule_full_house(*args) }
+    ones:      :rule_singles,
+    twos:      :rule_singles,
+    threes:    :rule_singles,
+    fours:     :rule_singles,
+    fives:     :rule_singles,
+    sixes:     :rule_singles,
+    pair:      :rule_pair,
+    two_pair:  :rule_two_pair,
+    fullhouse: :rule_full_house
   }.freeze
 
   def score(rolls, rule)
-    RULE_MAP[rule].call(rolls, rule)
+    method(RULE_MAP[rule])
+      .call(rolls, rule)
   end
 
   def rule_singles(rolls, rule)
@@ -52,7 +50,7 @@ module Yahtzee
 
   def rule_full_house(rolls, _rule)
     triple = rolls.find { |roll| rolls.count(roll) == 3 }
-    pairs  = pairs(rolls.reject { |roll| roll == triple})
+    pairs  = pairs(rolls.reject { |roll| roll == triple })
     pair   = pairs.first
 
     if triple && pair
